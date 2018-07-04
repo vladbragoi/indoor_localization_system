c7942d748e2e288ca3e1998ea570e7b9285f4764
vladbragoi <wireless95>
2018-07-04 16:01:37 +0200

    sample project

---
+++ app/src/main/java/it/univr/vlad/fingerprinting/Fingerprinting.java
@@ -0,0 +1,13 @@
+package it.univr.vlad.fingerprinting;
+
+import java.util.List;
+
+public class Fingerprinting {
+
+    Observer observer = new Observer() {
+        @Override
+        public void update(List results) {
+            // TODO: update values
+        }
+    };
+}
---
+++ app/src/main/java/it/univr/vlad/fingerprinting/wifi/MvManager.java
@@ -0,0 +1,28 @@
+package it.univr.vlad.fingerprinting.wifi;
+
+import it.univr.vlad.fingerprinting.Observer;
+import it.univr.vlad.fingerprinting.ScanResults;
+
+public class MvManager implements Manager, ScanResults {
+
+    @Override
+    public void bind() {
+
+    }
+
+    @Override
+    public void registerObserver(Observer scanResultsObserver) {
+
+    }
+
+    @Override
+    public void unregisterObserver(Observer scanResultsObserver) {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+
+    }
+
+}
